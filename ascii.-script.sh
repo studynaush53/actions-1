975e81e781285623489623cea68653631b2a287a
studynaush53 <150359437+studynaush53@users.noreply.github.com>
2024-02-12 07:47:03 +0530

    something

---
+++ ascii.-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get install cowsay -y 
+
+cowsay -f dragon "AM coming in hot" >> dragon.txt
+
+cat dragon.txt
+
+ls -ltr
+
+echo "ooooooooooooooo===============oooooooooooooooooooooo
+
